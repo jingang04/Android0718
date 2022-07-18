ede90ef03f246b8e010145095ef00eabf6c325c4
user <jinkang3724naver.com>
2022-07-18 09:01:12 +0900

    GitHub Test

---
+++ app/src/main/java/com/example/exam_shop2022/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.exam_shop2022;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import java.nio.file.Files;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
+
---
+++ app/src/main/java/com/example/exam_shop2022/SubActivity.java
@@ -0,0 +1,22 @@
+package com.example.exam_shop2022;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class SubActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub);
+
+        findViewById(R.id.button).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        finish();
+    }
+}
